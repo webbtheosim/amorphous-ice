74323ecb794285a825851cc95d6d0d6ec94c1a57
Quinn Gallagher <qg1361@princeton.edu>
2026-06-18 11:23:16 -0400

    Resetting config template.

--- src/config_template.py
+++ src/config_template.py
@@ -46,21 +46,6 @@ scan_paths = {
     'ice': [
         # ['/path/to/TrainingData/DP_SCAN/IceIh/ice_ih_100K.dump', -1, -1],
     ],
-    # Additional ice polymorphs (all use triclinic LAMMPS boxes):
-    # 'ice01c': [['/path/to/organized/ice01c/192atoms/trajectory.dump', -1, -1]],
-    # 'ice02':  [['/path/to/organized/ice02/288atoms/trajectory.dump',  -1, -1]],
-    # 'ice03':  [['/path/to/organized/ice03/288atoms/trajectory.dump',  -1, -1]],
-    # 'ice04':  [['/path/to/organized/ice04/384atoms/trajectory.dump',  -1, -1]],
-    # 'ice05':  [['/path/to/organized/ice05/168atoms/trajectory.dump',  -1, -1]],
-    # 'ice06':  [['/path/to/organized/ice06/240atoms/trajectory.dump',  -1, -1]],
-    # 'ice07':  [['/path/to/organized/ice07/384atoms/trajectory.dump',  -1, -1]],
-    # 'ice08':  [['/path/to/organized/ice08/192atoms/trajectory.dump',  -1, -1]],
-    # 'ice09':  [['/path/to/organized/ice09/288atoms/trajectory.dump',  -1, -1]],
-    # 'ice11':  [['/path/to/organized/ice11/384atoms/trajectory.dump',  -1, -1]],
-    # 'ice12':  [['/path/to/organized/ice12/288atoms/trajectory.dump',  -1, -1]],
-    # 'ice13':  [['/path/to/organized/ice13/168atoms/trajectory.dump',  -1, -1]],
-    # 'ice14':  [['/path/to/organized/ice14/288atoms/trajectory.dump',  -1, -1]],
-    # 'ice15':  [['/path/to/organized/ice15/240atoms/trajectory.dump',  -1, -1]],
 }
 
 mbpol_paths = {
@@ -73,19 +58,4 @@ mbpol_paths = {
     'ice': [
         # Hexagonal ice configurations (optional)
     ],
-    # Additional ice polymorphs (mix of orthogonal and triclinic boxes):
-    'ice01c': [['/path/to/organized/ice01c/648atoms/trajectory.dump',  -1, -1]],
-    'ice02':  [['/path/to/organized/ice02/2880atoms/trajectory.dump',  -1, -1]],
-    'ice03':  [['/path/to/organized/ice03/972atoms/trajectory.dump',   -1, -1]],
-    'ice04':  [['/path/to/organized/ice04/3072atoms/trajectory.dump',  -1, -1]],
-    'ice05':  [['/path/to/organized/ice05/2268atoms/trajectory.dump',  -1, -1]],
-    'ice06':  [['/path/to/organized/ice06/1920atoms/trajectory.dump',  -1, -1]],
-    'ice07':  [['/path/to/organized/ice07/1296atoms/trajectory.dump',  -1, -1]],
-    'ice08':  [['/path/to/organized/ice08/1536atoms/trajectory.dump',  -1, -1]],
-    'ice09':  [['/path/to/organized/ice09/972atoms/trajectory.dump',   -1, -1]],
-    'ice11':  [['/path/to/organized/ice11/1296atoms/trajectory.dump',  -1, -1]],
-    'ice12':  [['/path/to/organized/ice12/1944atoms/trajectory.dump',  -1, -1]],
-    'ice13':  [['/path/to/organized/ice13/2268atoms/trajectory.dump',  -1, -1]],
-    'ice14':  [['/path/to/organized/ice14/1620atoms/trajectory.dump',  -1, -1]],
-    'ice15':  [['/path/to/organized/ice15/1920atoms/trajectory.dump',  -1, -1]],
 }
